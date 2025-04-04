92920d12f402a3845579e88e3fa8f45f78b2e400
Sarthak Singh <sarthakankshawebjvb@gmail.com>
2025-04-05 03:25:47 +0530

    make contact with mail by any individuals, in portfolio, added backend

---
+++ backend/index.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const nodemailer = require("nodemailer");
+const cors = require("cors");
+const dotenv = require("dotenv");
+
+dotenv.config();
+if (!process.env.USER_MAIL || !process.env.MAIL_PASS) {
+    console.error("Missing USER or PASS in .env file");
+    process.exit(1);
+  }
+  
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.post("/sent-message", async (req, res) => {
+//   const { name, email, phone, email_sub, message } = req.body;
+
+
+  try {
+    const transporter = nodemailer.createTransport({
+     service: "gmail",
+
+      auth: {
+        user: process.env.USER_MAIL,
+        pass: process.env.MAIL_PASS,
+      },
+    });
+
+    const mailOption = {
+      from: process.env.USER_MAIL,
+      to: process.env.USER_MAIL,
+      replyTo: email,
+      subject: `Message from ${name} : ${email_sub}`,
+      text: `Name: ${name}\nEmail : ${email}\nPhone: ${phone}\nMessage: ${message}`,
+    };
+    const info = await transporter.sendMail(mailOption);
+    // console.log("Email Sent: ", info.response);
+
+    res
+      .status(200)
+      .json({ success: true, message: "Email sent successfully!" });
+  } catch (error) {
+    console.error("Email Send Error:", error);
+    res.status(500).json({ success: false, error: error.toString() });
+  }
+});
+
+app.listen(5000, () => {
+  console.log("Server is listening on port:5000");
+});
--- script.js
+++ script.js
@@ -1,20 +1,88 @@
-let menu = document.getElementById('menu-icon');
-let navbar = document.querySelector('.navbar');
+let menu = document.getElementById("menu-icon");
+let navbar = document.querySelector(".navbar");
 
 menu.onclick = () => {
-    menu.classList.toggle('bx-x');
-    navbar.classList.toggle('active');
-}
+  menu.classList.toggle("bx-x");
+  navbar.classList.toggle("active");
+};
 
 window.onscroll = () => {
-    menu.classList.remove('bx-x');
-    navbar.classList.remove('active');
-}
+  menu.classList.remove("bx-x");
+  navbar.classList.remove("active");
+};
 
-const typed = new Typed('.multiple-text', {
-    strings: ['Web Developer', 'Web Designer','Frontend Developer', 'Backend Developer', 'MERN Stack Developer'],
-    typeSpeed: 80,
-    backSpeed:80,
-    backDelay:1200,
-    loop:true,
-  });
+const typed = new Typed(".multiple-text", {
+  strings: [
+    "Web Developer",
+    "Web Designer",
+    "Frontend Developer",
+    "Backend Developer",
+    "MERN Stack Developer",
+  ],
+  typeSpeed: 80,
+  backSpeed: 80,
+  backDelay: 1200,
+  loop: true,
+});
+
+// contact
+
+const form = document.getElementById("contactForm");
+const notification = document.getElementById("notification");
+const notificationText = document.getElementById("notification-text");
+const progressbar = document.getElementById("progress-bar");
+
+form.addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  const formData = new FormData(form);
+  const data = Object.fromEntries(formData.entries());
+
+  try {
+    const res = await fetch("http://localhost:5000/sent-message", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+
+    const result = await res.json();
+
+    if (res.ok) {
+      ShowNotification(result.message, "success");
+      form.reset();
+    } else {
+      ShowNotification(result.message || "Something went wrong,", "error");
+    }
+  } catch (err) {
+    ShowNotification("Failed to send message. Please try again.", "error");
+  }
+});
+
+
+
+function ShowNotification(message, type) {
+  notificationText.innerText = message;
+  notification.style.display = "block";
+  notificationText.style.color = type === "success" ? "#28a745" : "#dc3545";
+  notification.style.opacity = 1;
+
+  // Reset Progress bar
+  progressbar.style.transition = "none";
+  progressbar.style.width = "0%";
+
+  setTimeout(() => {
+    progressbar.style.transition = "width 5s linear";
+    progressbar.style.width = "100%";
+  }, 50);
+
+  setTimeout(() => {
+    notification.style.opacity = 0;
+    setTimeout(() => {
+      notification.style.display = "none";
+      progressbar.style.transition = "none";
+      progressbar.style.width = "0%";
+    }, 300); // Fade out 
+  }, 5000);
+}
